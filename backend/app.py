6ceeb002e797ae8f1e15ff57be1fe5cc5cd21634
Krzysztof Pala <pallovsky@gmail.com>
2022-12-10 14:48:05 +0100

    Adding database connection and automatic migration execution

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-hotel = Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/hotel"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# This import is needed to be here for models be detected by flask migrate.
+from models import *
 
 
-@hotel.route('/')
+@app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
 
 if __name__ == "__main__":
-    hotel.run(debug=True)
+    app.run(debug=True)
---
+++ backend/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from sqlalchemy.dialects.postgresql import UUID
+
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'Users'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    role = db.Column(db.Integer())
+
+    def __init__(self, username, password, role):
+        self.id = uuid.uuid4()
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def __repr__(self):
+        return f"<User {self.username}, role {self.role}>"
